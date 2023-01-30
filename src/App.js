72c753a3238efdeaf295ab6b6133455554910ca3
Ankit Nayak <ankitnayak882@gmail.com>
2023-01-30 23:17:31 +0530

    Code Push

--- src/App.js
+++ src/App.js
@@ -1,94 +1,67 @@
-import './App.css';
-import './index.css';
-import React from 'react';
-import { useState } from 'react';
-import TodoList from './TodoList';
+import "./App.css";
+import "./index.css";
+import React from "react";
+import { useState } from "react";
+import TodoList from "./TodoList";
 
 const App = () => {
+	const [todo, setTodo] = useState("");
+	const [todos, setTodos] = useState([]);
 
+	const addTodos = (e) => {
+		e.preventDefault();
+		if (todo === "") alert("Enter Your Todo First ");
+		else {
+			setTodos((oldItems) => {
+				return [...oldItems, todo];
+			});
+			setTodo("");
+		}
+	};
 
-  const [inputList, setInputList] = useState(""); //Taking Initial input And Updating 
-  const [items, setItems] = useState([]); // Adding those input as empty array  
+	const deleteTodo = (id) => {
+		setTodos((oldItems) => {
+			return oldItems.filter((arrElement, index) => {
+				return index !== id;
+			});
+		});
+	};
 
-  const seeChangeInput = (event) => { //Adding the user value to the inputList
-    setInputList(event.target.value);
-  }
-
-  const addListItems = () => { //adding the list every timr to our previous array
-    var a = document.getElementById('hi').value; //Checking For Empty Value
-    if (a === '') alert("Enter Your Todo First ");
-    /* if(!inputList) {
-      '{alert}'
-    } */
-    else {
-      setItems((oldItems) => {
-        return ([...oldItems, inputList]);
-      })
-      setInputList("");
-    }
-  }
-
-  //Deleting items on select / ON click
-  /* Since take id from icon click and match with all the id of Array , return only the id that do  not matches  */
-  const deleteTodo = (id) => {
-    //console.log( " Deleted ");
-    setItems((oldItems) => {
-      return oldItems.filter((arrElement, index) => {
-        return index !== id;
-      })
-    })
-  }
-
-  const removeAll =() => {
-    setItems([]);
-  }
-
-
-  return (
-    <>
-      <div className="main-div">
-        <div className="center-div">
-          <br />
-          <h1> ToDo List</h1>
-          <br />
-          <input id='hi' type="text"
-            placeholder='Add your Todo item '
-            onChange={seeChangeInput}
-            value={inputList}
-          />
-          <button className='bye' onClick={addListItems}  > + </button>
-
-          <ol>
-            {/* <li>{inputList}</li> */}
-            {
-              items.map((itemsVal, index) => {
-                // return (<li>{itemsVal}</li>)
-                return (<TodoList
-                  key={index}
-                  // itemsVal ={itemsVal}
-                  text={itemsVal}
-                  id={index}
-
-                  onSelect={deleteTodo}
-                />)
-              })
-            }
-          </ol>
-        </div>
-        <br />
-        <div className='footer'> Total Items You Need To Do : {items.length} </div>
-
-        <div className='listall'
-        // data-sm-link-text='Remove ALl'
-        onClick={removeAll}
-        >
-          <button className='list1' >
-            Remove All
-          </button> </div>
-      </div>
-
-    </>
-  )
-}
+	return (
+		<>
+			<div className="main-div">
+				<div className="center-div">
+					<br />
+					<h1> ToDo List</h1>
+					<br />
+					<form>
+						<input
+							type="text"
+							placeholder="Add your Todo item"
+							onChange={(e) => setTodo(e.target.value)}
+							value={todo}
+						/>
+						<button className="btn-add" onClick={addTodos}>
+							+
+						</button>
+					</form>
+					<ol>
+						{todos.map((todo, index) => {
+							return (
+								<TodoList
+									key={index}
+									text={todo}
+									id={index}
+									deleteTodo={deleteTodo}
+								/>
+							);
+						})}
+					</ol>
+				</div>
+				<br />
+			</div>
+		</>
+	);
+};
 
 export default App;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,25 +1,19 @@
 import React from "react";
 
 const TodoList = (props) => {
-
-/*     const deleteTodo = () => {
-        console.log( " Deleted ");
-    } */
-
-    return (
-        <>
-            <div className="todo-style">
-                <i className= "fa fa-times" aria-hidden="true" 
-                // onClick={ deleteTodo}
-                onClick={ 
-                    () => {
-                        props.onSelect(props.id); 
-                    }
-                }
-                />
-                <li>{props.text}</li>
-            </div>
-        </>
-    )
+	return (
+		<>
+			<div className="todo">
+				<i
+					className="fa fa-times"
+					// aria-hidden="true"
+					onClick={() => {
+						props.deleteTodo(props.id);
+					}}
+				/>
+				<li>{props.text}</li>
+			</div>
+		</>
+	);
 };
 export default TodoList;
